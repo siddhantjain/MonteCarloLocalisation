43553ce2c878ff6069f96309371e249e142cfcc8
Siddhant <post.siddhant@gmail.com>
2018-02-05 18:29:25 -0500

    adding skeleton code for sensor model

--- scripts/SensorModel.py
+++ scripts/SensorModel.py
@@ -15,10 +15,34 @@ class SensorModel:
     """
 
     def __init__(self, occupancy_map):
-
+        self._occupancy_map = occupancy_map;
+        self.zHit = 1;
+        self.zShort = 1;
+        self.zMax = 1;
+        self.zRand = 1;
         """
         TODO : Initialize Sensor Model parameters here
         """
+    def raycast(self, pos,angle):
+        range = 0;
+
+        return range
+    def calcPHit(self):
+        pHit = 1
+
+        return pHit
+
+    def calcPShort(self):
+        pShort =1
+        return pShort
+
+    def calcPMax(self):
+        pMax = 1
+        return pMax
+
+    def calcPRand(self):
+        pRand = 1
+        return pRand
 
     def beam_range_finder_model(self, z_t1_arr, x_t1):
         """
@@ -30,8 +54,19 @@ class SensorModel:
         """
         TODO : Add your code here
         """
-        # Adding the following one line for compiling starter code. Remove when implementing this
-        q =1;
+        q =1
+        i=0
+        #TODO: siddhant: write cleaner code. remove constant 180
+        for i in range(180):
+            if i%5 ==0:
+                z_k_star =  self.raycast(x_t1,i)
+                p_hit = self.calcPHit()
+                p_short = self.calcPShort()
+                p_max = self.calcPMax()
+                p_rand = self.calcPRand()
+
+                p = self.zHit*p_hit + self.zShort*p_short + self.zMax*p_max + self.zRand*p_rand
+                q = q*p
         return q    
  
 if __name__=='__main__':
